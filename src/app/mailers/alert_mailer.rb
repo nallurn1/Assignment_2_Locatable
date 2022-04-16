22b2049fc931fc7afb4318f112524467146240b2
Student 1 Account <student1@hpc272.tcnj.edu>
2022-04-16 09:56:58 -0400

    Updates for Sending Mail

--- src/app/mailers/alert_mailer.rb
+++ src/app/mailers/alert_mailer.rb
@@ -2,6 +2,6 @@ class ReminderMailer < ApplicationMailer
     def new_reminder_email
         @alert = params[:alert]
 
-        mail(to: @contact.user_email, subject: "ACCR Mail Reminder")
+        mail(to: @contact.user_email, subject: "Important Alert")
     end
 end
